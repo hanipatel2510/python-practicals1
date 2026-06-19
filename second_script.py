7c7a9ba5d5a600d890e39a22679526e9b06e69e2
hanee <hneepatel@gmail.com>
2026-06-19 22:19:49 +0530

    done

---
+++ second_script.py
@@ -0,0 +1,82 @@
+# Lab Work 1.2 
+# Q.1 Write a program to demonstrate different formatting options in print():
+# Use sep to separate values with a custom character.
+# Use end to customize what appears at the end of a print() statement.
+
+# print("Data analysis", "Python", sep=" | ")
+
+# print("Hello", end=" ")
+# print("World")
+
+# Q.2 Create a program that asks the user for their name, age,and favourite hobby using the input() function,
+# then displays a fromatted message.
+
+# name = input("Enter your name: ")
+# age = input("Enter your age: ")
+# hobby = input("Enter your favourite hobby: ")
+
+# print(f"Hello, {name}! at {age}, enjoying {hobby} sounds fun!")
+
+# Q.3 Perform addition, subtraction, multiplication,division, floor division, modulus, and 
+# exponentiation on two numbers inputby the user.
+
+# number1 = int(input("Enter first number: "))
+# number2 = int(input("Enter second number: "))
+
+# print("addition:", number1 + number2)
+# print("Subtraction:", number1 - number2)
+# print("Multiplication:", number1 * number2)
+# print("Division:", number1 / number2)
+# print("Floor Division:", number1 // number2)
+# print("Modulus:", number1 % number2)
+# print("Exponentiation:", number1 ** number2)
+
+# Q.4 write a python program that:
+# Declare variables of different datatypes(integer,float,string,boolean,complex,etc).
+# print their values and types using the type() function.
+
+# num1= 10
+# num2= 10.5
+# num3 = "Python"
+# num4 = True
+# num5 = 3 + 4j
+
+# print(num1, type(num1))
+# print(num2, type(num2))
+# print(num3, type(num3))
+# print(num4, type(num4))
+# print(num5, type(num5))
+
+# Q.5 Create a program where the user inputs their height and weight.
+# store them in appropriately named variables  and print a formatted message displaying their value.
+
+# height = float(input("Enter your height (in cm): "))
+# weight = float(input("Enter your weight (in kg): "))
+# print(f"Your height is {height} cm and your weight is {weight} kg.")
+
+# Q.6 Implement a program to demonstrate logical operators (and, or, not)by asking the user for boolean inputs.(e.g.,true/false valuese).
+
+# a1 = input("Enter True or False: ") == "True"
+# b1 = input("Enter True or False: ") == "True"
+
+# print("AND :", a1 and b1)
+# print("OR  :", a1 or b1)
+# print("NOT a1 :", not a1)
+
+# Q.7 Write a program to demonstrate assignment operators (=, +=, -=, *=, /=) using a single variable.
+
+x = 10
+print("value =", x)
+
+x += 5
+print("+= 5 :", x)
+
+x -= 3
+print("-= 3 :", x)
+
+x *= 2
+print("*= 2 :", x)
+
+x /= 4
+print(" /= 4 :", x) 
+
